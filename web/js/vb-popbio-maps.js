f8165460dee3d9ea5c3835e77cd730dc98396568
ikirmitz <ioanniskirmitzoglou@gmail.com>
2016-02-03 17:33:17 +0000

    VB-4081 PopBio map: open map page from link that enables "IR mode" (and other functionality) use ?view=ir to land directly on IR view

--- web/js/vb-popbio-maps.js
+++ web/js/vb-popbio-maps.js
@@ -66,26 +66,8 @@ function initializeMap() {
     layerCtl.setPosition('topright');
     layerCtl.addTo(map);
 
-    //    Add a legend control on the bottom right but don't show it just yet
-    //legend = L.control({position: 'bottomright'});
-    //legendDiv = L.DomUtil.create('div', 'info legend');
 
-
-    //L.easyButton('fa-info',
-    //    function () {
-    //        if (L.DomUtil.hasClass(legendDiv, "active")) {
-    //            legend.removeFrom(map);
-    //            L.DomUtil.removeClass(legendDiv, "active");
-    //        } else {
-    //            legend.addTo(map);
-    //            L.DomUtil.addClass(legendDiv, "active");
-    //        }
-    //
-    //    },
-    //    'Toggle legend ON of OFF'
-    //);
-
-    // Now generate the legend and .
+    // Now generate the legend
 
     var url = solrPopbioUrl + $('#view-mode').val() + 'Palette?q=*&facet.pivot=geohash_2,species_category&json.wrf=?&callback=?';
     //console.log('requesting url ' + url);
@@ -1745,3 +1727,148 @@ Number.prototype.roundDecimals = function (decimals) {
     }
 
 };
+
+
+// Add an URL parser to JQuery that returns an object
+// This function is meant to be used with an URL like the window.location
+// Use: $.parseParams('http://mysite.com/?var=string') or $.parseParams() to parse the window.location
+// Simple variable:  ?var=abc                        returns {var: "abc"}
+// Simple object:    ?var.length=2&var.scope=123     returns {var: {length: "2", scope: "123"}}
+// Simple array:     ?var[]=0&var[]=9                returns {var: ["0", "9"]}
+// Array with index: ?var[0]=0&var[1]=9              returns {var: ["0", "9"]}
+// Nested objects:   ?my.var.is.here=5               returns {my: {var: {is: {here: "5"}}}}
+// All together:     ?var=a&my.var[]=b&my.cookie=no  returns {var: "a", my: {var: ["b"], cookie: "no"}}
+// You just cant have an object in an array, ?var[1].test=abc DOES NOT WORK
+// Taken from https://gist.github.com/kares/956897 (scroll down, code in the comments)
+(function ($) {
+    var re = /([^&=]+)=?([^&]*)/g;
+    var decode = function (str) {
+        return decodeURIComponent(str.replace(/\+/g, ' '));
+    };
+    $.parseParams = function (query) {
+        // recursive function to construct the result object
+        function createElement(params, key, value) {
+            key = key + '';
+            // if the key is a property
+            if (key.indexOf('.') !== -1) {
+                // extract the first part with the name of the object
+                var list = key.split('.');
+                // the rest of the key
+                var new_key = key.split(/\.(.+)?/)[1];
+                // create the object if it doesnt exist
+                if (!params[list[0]]) params[list[0]] = {};
+                // if the key is not empty, create it in the object
+                if (new_key !== '') {
+                    createElement(params[list[0]], new_key, value);
+                } else console.warn('parseParams :: empty property in key "' + key + '"');
+            } else
+            // if the key is an array
+            if (key.indexOf('[') !== -1) {
+                // extract the array name
+                var list = key.split('[');
+                key = list[0];
+                // extract the index of the array
+                var list = list[1].split(']');
+                var index = list[0]
+                // if index is empty, just push the value at the end of the array
+                if (index == '') {
+                    if (!params) params = {};
+                    if (!params[key] || !$.isArray(params[key])) params[key] = [];
+                    params[key].push(value);
+                } else
+                // add the value at the index (must be an integer)
+                {
+                    if (!params) params = {};
+                    if (!params[key] || !$.isArray(params[key])) params[key] = [];
+                    params[key][parseInt(index)] = value;
+                }
+            } else
+            // just normal key
+            {
+                if (!params) params = {};
+                params[key] = value;
+            }
+        }
+
+        // be sure the query is a string
+        if (!query) {
+            query = window.location + '';
+        } else {
+            query = query + '';
+
+        }
+
+        var params = {}, e;
+        if (query) {
+            // remove # from end of query
+            if (query.indexOf('#') !== -1) {
+                query = query.substr(0, query.indexOf('#'));
+            }
+
+            // remove ? at the begining of the query
+            if (query.indexOf('?') !== -1) {
+                query = query.substr(query.indexOf('?') + 1, query.length);
+            } else return {};
+            // empty parameters
+            if (query == '') return {};
+            // execute a createElement on every key and value
+            while (e = re.exec(query)) {
+                var key = decode(e[1]);
+                var value = decode(e[2]);
+                createElement(params, key, value);
+            }
+        }
+        return params;
+    };
+})(jQuery);
+
+
+// Encode an object to an url string
+// This function return the search part, beginning with "?"
+// Use: $.encodeURL({var: "test", len: 1}) returns ?var=test&len=1
+(function ($) {
+    $.encodeURL = function (object) {
+
+        // recursive function to construct the result string
+        function createString(element, nest) {
+            if (element === null) return '';
+            if ($.isArray(element)) {
+                var count = 0,
+                    url = '';
+                for (var t = 0; t < element.length; t++) {
+                    if (count > 0) url += '&';
+                    url += nest + '[]=' + element[t];
+                    count++;
+                }
+                return url;
+            } else if (typeof element === 'object') {
+                var count = 0,
+                    url = '';
+                for (var name in element) {
+                    if (element.hasOwnProperty(name)) {
+                        if (count > 0) url += '&';
+                        url += createString(element[name], nest + '.' + name);
+                        count++;
+                    }
+                }
+                return url;
+            } else {
+                return nest + '=' + element;
+            }
+        }
+
+        var url = '?',
+            count = 0;
+
+        // execute a createString on every property of object
+        for (var name in object) {
+            if (object.hasOwnProperty(name)) {
+                if (count > 0) url += '&';
+                url += createString(object[name], name);
+                count++;
+            }
+        }
+
+        return url;
+    };
+})(jQuery);
