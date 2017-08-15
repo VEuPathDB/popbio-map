9f681e7b4fc4419e9c8a017295ab6b74fbf4d3b5
Antelmo Aguilar <aaguilar@localhost.localdomain>
2017-08-15 11:31:16 -0400

    VB-6751: Initial commit on progress for prototype of PopBio map abundance data graph

---
+++ web/js/vb-highcharts.js
@@ -0,0 +1,212 @@
+(function (PopulationBiologyMap, $, undefined) {
+
+    if (PopulationBiologyMap.data == undefined) {
+        PopulationBiologyMap.data = {};
+    }
+
+    PopulationBiologyMap.data.highcharts = {};
+    
+    if (PopulationBiologyMap.methods == undefined) {
+        PopulationBiologyMap.methods = {};
+    }
+
+
+    //Tasks that need to be done or events defined  when the page loads
+   PopulationBiologyMap.init = function() {
+
+        console.log("here");
+    }
+
+
+    PopulationBiologyMap.methods.qs = function(key) {
+        key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&"); // escape RegEx meta chars
+        var match = location.search.match(new RegExp("[?&]"+key+"=([^&]+)(&|$)"));
+        return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+    }
+
+    PopulationBiologyMap.methods.createProjectSelect = function(divid, filter) {
+        //How the URL will be constructed
+        //var abundanceUrl = solrPopbioUrl + 'abndProjects?&
+        var abundanceUrl = "/popbio/qsolr/solr/vb_popbio/query?";
+        var queryParameters = "q=has_abundance_data_b:true%20AND%20-sample_size_i:0&rows=0&wt=json&facet=true&facet.mincount=1&facet.field=projects_category&fq=bundle:pop_sample";
+        var queryUrl = abundanceUrl + queryParameters + filter; // + "&json.wrf=?&callback=?";
+
+        PopulationBiologyMap.data.filter = filter;
+
+        $('#swarm-chart-area').fadeOut();
+
+        //$.getJSON(url)
+        //    .done(function (json) {
+        $.ajax({
+            type: 'GET',
+            url: queryUrl,
+            beforeSend: function(xhr) {
+                //Clear chart area and start the spinner
+                PaneSpin('swarm-plots', 'start');
+                $('#swarm-chart-area').empty();
+                $(divid + ' select').remove();
+                $(divid + ' label').remove();
+
+                //Probably not nessary
+                if (xhr && xhr.overrideMimeType) {
+                    xhr.overrideMimeType('application/json;charset-utf-8');
+                }
+            },
+            dataType: 'json',
+            success: function (json) {
+                console.log(json);
+                var projects_list = json.facet_counts.facet_fields.projects_category;
+                if (projects_list && Object.keys(projects_list).length > 0) {
+                    setTimeout(function () {
+                        //Create the dropdown
+                        var label = $('<label>').text('Select Project to View Abundance Graph: ');
+                        var aps = $('<select />').attr('id', 'agSelectProject')
+                            .attr('class', 'form-control');
+
+                        projects_list.forEach(function (project) {
+                            if (typeof project == 'string') {
+                                console.log(project);
+                                $('<option/>', {
+                                    text: project,
+                                    value: project,
+                                }).appendTo(aps);
+                                label.appendTo($(divid));
+                                aps.appendTo($(divid));
+                            }
+                        });
+
+                        //Build graph using the first option in the drop-down
+                        console.log("Adding graph");
+                        var project_id = aps.find(':selected').val();
+                        //Get graph data for project and build chart                
+                        PopulationBiologyMap.data.selected_project = project_id;
+                        PopulationBiologyMap.data.highcharts[project_id] = [];
+
+                        //Construct URL used to retrieve data from solr
+                        var abundanceUrl = "/popbio/qsolr/solr/vb_popbio/query?";
+                        var query = "q=has_abundance_data_b:true%20AND%20-sample_size_i:0%20AND%20projects_category:" + project_id;
+                        var json_facet = '&json.facet={species:{type:terms,limit:-1,field:species_category,facet:{collection_dates:{type:terms,limit:-1,sort:index,field:collection_date,facet:{sumAbnd:"sum(div(sample_size_i,collection_duration_days_i))"}}}}}';
+                        var queryParameters = query + "&rows=0&wt=json";
+                        //Get filter of the record
+                        var filter = PopulationBiologyMap.data.filter + "&fq=bundle:pop_sample";
+                        var queryUrl = abundanceUrl + queryParameters + json_facet + filter; 
+
+                        $.ajax({
+                            url: queryUrl,
+                            dataType: 'json',
+                            success: function(json) {
+                                console.log(json);
+                                //Get species collection info from response
+                                var species_collections_list = json.facets.species.buckets;
+
+                                species_collections_list.forEach(function (species_collections) {
+                                    console.log(species_collections);
+                                                                            
+                                    //Used to hold the formatted data for a single species chart
+                                    PopulationBiologyMap.data.single_species_data = {"name": species_collections.val, "data": []};
+                                    collections_info = species_collections.collection_dates.buckets;
+                                    collections_info.forEach(function (collection) {
+                                        var unix_date = new Date(collection.val).getTime();
+                                        PopulationBiologyMap.data.single_species_data.data.push([unix_date, collection.sumAbnd]); 
+                                        console.log(collection);
+                                    });
+                                    //Putting project_id in local variable to not type out original
+                                    var project_id = PopulationBiologyMap.data.selected_project;
+                                    PopulationBiologyMap.data.highcharts[project_id].push(PopulationBiologyMap.data.single_species_data);
+                                });
+                            },
+                            error: function() {
+                                //PaneSpin('swarm-plots', 'stop');
+                                console.log("An error has occurred");
+                            },
+                            complete: function() {
+                                //Construct graph with ajax call to Solr servr
+                                data = PopulationBiologyMap.data.highcharts[project_id];
+                                PopulationBiologyMap.methods.createStockchart(data); 
+                                PaneSpin('swarm-plots', 'stop');
+                                $('#swarm-chart-area').show()
+                            }
+                        });
+                    }, delay);
+                }
+            },
+            error: function() {
+                //PaneSpin('swarm-plots', 'stop');
+                console.log("An error has occurred");
+            }
+        });
+    }
+
+    PopulationBiologyMap.methods.createStockchart = function(data) {
+        $('#swarm-chart-area').highcharts('StockChart',{
+/*            xAxis: {
+                labels: {
+                    formatter: function () {
+                        return this.value;
+                    }
+                }
+            },
+            rangeSelector: {
+                enabled: false
+            },*/
+            legend: {
+                enabled: true
+            },
+           /* navigator: {
+                xAxis: {
+                    labels: {
+                        formatter: function () {
+                            return this.value;
+                        }
+                    }
+                },
+                series: {
+                    data: []
+                },
+                height: 20
+            },*/
+            /*tooltip: {
+                formatter: function () {
+                    var headerHTML = '<span style="font-size: 10px">Year '+ this.x;
+                    var dataGrouped = false;
+                    var footerHTML = '';
+
+                    //Sort the tooltip points from largest value to lowest
+                    var sortedPoints = this.points.sort(
+                        function(a, b) {
+                            return ((a.y > b.y) ? -1 : ((a.y < b.y) ? 1 : 0));
+                        }
+                    );
+
+
+                    //$.each(this.points, function (i, point) {
+                    $.each(sortedPoints, function (i, point) {
+                        if (point.series.hasGroupedData) {
+                            dataGrouped = true;
+                        }
+                        footerHTML += '<br/><span style="color: ' + point.series.color + '">' +
+                            point.series.name + ':</span><b>' + point.y + '</b>';
+                    });
+
+                    if (dataGrouped) {
+                        headerHTML += '  <i>(Grouped Data)</i></span><br/>'
+                    } else {
+                       headerHTML += '</span><br/>';
+                    }
+
+                    return headerHTML + footerHTML;
+                }  
+            },*/
+	        /*series: [{
+		    type: 'column',
+		    name: 'USD to EUR',
+		    data: [[1990,10]]
+	        }]*/
+            series: data
+        });
+    };
+
+    $(document).ready(function () {
+        PopulationBiologyMap.init();
+    });
+})(window.PopulationBiologyMap = window.PopulationBiologyMap || {}, jQuery);
--- web/js/vb-popbio-maps.js
+++ web/js/vb-popbio-maps.js
@@ -1611,8 +1611,12 @@ function loadSolr(parameters) {
                                         panel.data('has-graph', true);
                                         break;
                                     case "swarm-plots":
-                                        createBeeViolinPlot("#swarm-chart-area", buildBbox(recBounds));
-                                        panel.data('has-graph', true);
+                                        if (PopulationBiologyMap.methods.qs('view') === 'abnd') {
+                                            PopulationBiologyMap.methods.createProjectSelect("#swarm-plots", buildBbox(recBounds));
+                                        } else {
+                                          createBeeViolinPlot("#swarm-chart-area", buildBbox(recBounds));
+                                          panel.data('has-graph', true);
+                                        } 
                                         break;
                                     case "marker-table":
                                         updateTable("#table-contents", buildBbox(recBounds));
@@ -1636,7 +1640,9 @@ function loadSolr(parameters) {
                                         }
                                         break;
                                     case "swarm-plots":
-                                        if (!panel.data('has-graph')) {
+					if (PopulationBiologyMap.methods.qs('view') === 'abnd') {
+                                            PopulationBiologyMap.methods.createProjectSelect("#swarm-plots", buildBbox(recBounds));
+                                        } else if (!panel.data('has-graph')) {
                                             createBeeViolinPlot("#swarm-chart-area", buildBbox(recBounds));
                                             panel.data('has-graph', true);
                                         }
@@ -1690,8 +1696,14 @@ function loadSolr(parameters) {
                                             panel.data('has-graph', true);
                                             break;
                                         case "swarm-plots":
-                                            createBeeViolinPlot("#swarm-chart-area", buildBbox(recBounds));
-                                            panel.data('has-graph', true);
+                                            if (PopulationBiologyMap.methods.qs('view') === 'abnd') {
+                                                //var data = [{"data": [[1951, 1308], [1951, 0], [1952, 1201], [1953, 937], [1954, 1037], [1955, 1023], [1956, 959], [1957, 928], [1958, 1024], [1959, 814], [1960, 939], [1961, 1114], [1962, 1563], [1963, 948], [1964, 995], [1965, 1038], [1966, 892], [1967, 1153], [1968, 821], [1969, 930], [1969, 0]], "name": "Elkhart"}, {"data": [[1951, 1579], [1952, 1741], [1953, 1465], [1954, 1072], [1955, 1122], [1956, 1025], [1957, 1414], [1958, 1338], [1959, 1163], [1960, 1715], [1961, 1230], [1962, 1523], [1963, 1525], [1964, 1618], [1965, 1368], [1966, 1131], [1967, 1045], [1968, 1343], [1969, 1024]], "name": "South Bend"}];
+                                                PopulationBiologyMap.methods.createProjectSelect("#swarm-plots", buildBbox(recBounds));
+
+                                            } else {
+                                                createBeeViolinPlot("#swarm-chart-area", buildBbox(recBounds));
+                                                panel.data('has-graph', true);
+                                            }
                                             break;
                                         case "marker-table":
                                             updateTable("#table-contents", buildBbox(recBounds));
