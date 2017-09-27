VEuPathDB/popbio-map

commit 46f98e7736819511a54f0d835ba4619f7073f5ae

marker and pie numbers are all sum(sample_size_i), zeroes now included in sums, pie chart sizing now better.  numbers above 999 are 10k etc

web/js/vb-popbio-maps.js
@@ -1409,8 +1409,11 @@ function loadSolr(parameters) {
 
 
         // display the number of results
+
         if (viewMode === "ir") {
             $("#markersCount").html(result.response.numFound + ' visible assays summarized by ' + glbSummarizeBy + '</u>');
+        } else if (viewMode === "abnd") {
+            $("#markersCount").html(result.facets.sumSmp + ' visible individuals summarized by ' + glbSummarizeBy + '</u>');
         } else {
             $("#markersCount").html(result.response.numFound + ' visible samples summarized by ' + glbSummarizeBy + '</u>');
         }
@@ -1439,42 +1442,26 @@ function loadSolr(parameters) {
             var key = el.val,
                 elStats = [],
                 fullElStats = [],
-                geoCount = el.count,
-                tagsTotalCount = 0,
-                geoAvgAbnd = 0;
-
-            if (viewMode === 'abnd') {
-
-                geoAvgAbnd = el.avgAbnd; // colour markers by average abundance
-            }
-            ;
+                geoCount = viewMode === 'abnd' ? el.sumSmp : el.count,
+                tagsTotalCount = 0;
 
             var geoTerms = el.term.buckets;
 
             geoTerms.forEach(function (inEl) {
                 var inKey = inEl.val;
-                if (legend.options.summarizeBy === 'Species') {
+                var inCount = viewMode === 'abnd' ? inEl.sumSmp : InEl.count;
 
-                    //FixMe: Remove these replacements when proper names are returned from the popbio API
-                    fullElStats.push({
-                        "label": inKey.replace(/sensu lato/, "sl")
-                            .replace(/chromosomal form/, "cf"),
-                        // store normalised abundance for abundance mode, else store samples/assay counts
-                        "value": inEl.count,
-                        "color": (legend.options.palette[inKey] ? legend.options.palette[inKey] : "#000000")
-                    });
-                } else {
+                if (inCount > 0) {
                     fullElStats.push({
                         "label": inKey,
                         // store normalised abundance for abundance mode, else store samples/assay counts
-                        "value": inEl.count,
+                        "value": inCount,
                         "color": (legend.options.palette[inKey] ? legend.options.palette[inKey] : "#000000")
                     });
                 }
-                ;
 
                 // store the total counts
-                tagsTotalCount += inEl.count;
+                tagsTotalCount += inCount;
                 // if (viewMode === 'abnd') geoAbndSum += inEl.avgAbnd;
 
             });
@@ -1497,7 +1484,7 @@ function loadSolr(parameters) {
             // arr.cumulativeCount = tagsTotalCount
             arr.count = geoCount;
             if (viewMode === 'abnd') {
-                arr.normAbnd = geoAvgAbnd.roundDecimals(1);
+                // arr.normAbnd = geoAvgAbnd.roundDecimals(1);
                 // arr.avgDuration = el.avgDur.roundDecimals(1);
                 // arr.avgSampleSize = el.avgSmp.roundDecimals(1);
                 // arr.abndSum = geoAbndSum;
@@ -1511,9 +1498,6 @@ function loadSolr(parameters) {
             }
             if (viewMode === 'ir') {
                 arr.trafficlight = el.irAvg;
-            } else if (viewMode === 'abnd') {
-                arr.trafficlight = Math.log10(el.avgAbnd);
-                console.log(arr.trafficlight);
             } else {
                 arr.trafficlight = -1;
             }
@@ -1564,10 +1548,14 @@ function loadSolr(parameters) {
             setIcon: function (record) {
                 // if in abundance mode then resize the icons based on the samples count
                 var size = 40;
-                if (viewMode === 'abnd') { size = 60 };
+                // if (viewMode === 'abnd') { size = 60 };
+                var markerText = record.count;
+                if (viewMode === 'abnd' && record.count > 999) {
+                    markerText = Math.round(record.count/1000)+"k";
+                }
                 return new L.Icon.Canvas({
                     iconSize: new L.Point(size, size),
-                    markerText: record.count,
+                    markerText: markerText,
                     count: record.count,
                     trafficlight: record.trafficlight,
                     id: record.term,
@@ -2037,10 +2025,12 @@ function updatePieChart(population, stats) {
 
         $('#pie-chart-header').empty();
 
+        var vSize = 400 + 10*stats.length;
+
         d3.select("#pie-chart-area svg")
             .attr("width", "100%")
-            .attr("height", "500px")
-            .style({width: "100%", height: "500px"});
+            .attr("height", vSize+"px")
+            .style({width: "100%" , height: vSize+"px"});
 
         nv.addGraph(function () {
             var chart = nv.models.pieChart()

.gitignore
@@ -49,7 +49,7 @@ com_crashlytics_export_strings.xml
 crashlytics.properties
 crashlytics-build.properties
 /web/hpc-leap/
-web/libs/jquery-endless-scroll/
+web/libs
 /web/malaria/
 SOLR/genea_expression/
 web/genea_expression.html

web/js/map.Legend.js
@@ -498,8 +498,8 @@ L.Control.MapLegend = L.Control.extend({
                 '<span class="active-others" data-toggle="modal" data-target="#ir-normalisation-help">' +
                 'More info</span></p>';
 
-        } else if (viewMode === 'abnd') {
-            // abundance view
+        } else if (viewMode === 'NOTabnd') {
+            // abundance view // SHADING IS DISABLED FOR NOW
             inHtml += '<div class="data-layer-legend" style="border: 0">';
             inHtml += '<p style="text-align: left">Average abundance</p>';
             inHtml += '<div id="legend-ir-scale-bar">';
@@ -649,13 +649,12 @@ L.Control.MapLegend = L.Control.extend({
         if (viewMode === 'ir') {
 
             trafficlight.scale = new L.CustomColorFunction(0, 1, options.trafficlight.colorBrewer, {interpolate: true});
-        } else if (viewMode === 'abnd') {
+        } else if (viewMode === 'NOTabnd') { // not currently showing a colour scale in legend
             trafficlight.scale = new L.CustomColorFunction(0, Math.log(maxAbnd), options.trafficlight.colorBrewer, {interpolate: true});
-
-
+            trafficlight.min = 0;
+            trafficlight.max = maxAbnd;
         }
-        trafficlight.min = 0;
-        trafficlight.max = maxAbnd;
+
 
 
         // this is where the legend items are scored and sorted based on their frequency/abundance

commit 87934e38e5c7f45d187273d6d05df2e5ac0b8b0c

left hand side details tables weren't showing in abundance view. fixed the bug (introduced by https://bitbucket.org/vectorbase/popbio-map/commits/46f98e7736819511a54f0d835ba4619f7073f5ae )

web/js/vb-popbio-maps.js
@@ -2296,7 +2296,6 @@ function tableHtml(divid, results) {
                 };
 
                 row.smplAvgAbnd = row.sampleSize / row.collectionDuration;
-                [row.smplAvgAbndBgColor, row.smplAvgAbndTextColor] = legend.markerColor(Math.log10(row.smplAvgAbnd));
 
                 template = $.templates("#abndRowTemplate");
 